dd70484dae4c7f48989eb622e52f39d15d65c42d
sleepyDev0x <cuihsgg@qq.com>
2025-11-19 08:55:30 +0800

    update v2

--- version_2/include/CentralCache.h
+++ version_2/include/CentralCache.h
@@ -13,6 +13,19 @@ public:
         static CentralCache instance;
         return instance;
     }
+    // 从中心缓存获得内存块
+    void* fetchRange(size_t index);
+    void returnRange(void* start,size_t size,size_t index);
+
+private:
+    CentralCache(); // 构造函数为私有 服务于单例
+
+    // 从页缓存获取内存
+    void* fetchFromPageCache(size_t size);
+
+private:
+    std::array<std::atomic<void*>,FREE_LIST_SIZE> centralFreeList_; // 中心缓存的空闲链表 - 也是每种大小的内存块对应一个空闲链表 中心缓存可能会被多线程访问，因此为atomic<void*>类型
+
 
 
 };
--- version_2/src/CentralCache.cpp
+++ version_2/src/CentralCache.cpp
@@ -5,5 +5,17 @@
 #include "../include/CentralCache.h"
 
 namespace MemoryPool {
+    CentralCache::CentralCache() {
+        // 所有大小的内存块对应的空闲链表，初始为空 memory_order_relaxed - 不保证顺序，仅保证原子性
+        for (auto& ptr : centralFreeList_) {
+            ptr.store(nullptr,std::memory_order_relaxed);
+        }
+
+    }
+
+    void *CentralCache::fetchRange(size_t index) {
+        return nullptr;
+    }
+
 
 } // MemoryPool
