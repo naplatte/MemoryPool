4ef8ae08af3021d43f17dd53838a6a079a30c8c6
sleepyDev0x <cuihsgg@qq.com>
2025-12-04 15:37:43 +0800

    fetchRange

--- version_2/src/CentralCache.cpp
+++ version_2/src/CentralCache.cpp
@@ -5,7 +5,7 @@
 #include "../include/PageCache.h"
 
 namespace MemoryPool {
-    static const size_t SPANPAGES = 8; // 每次从页缓存获取的span大小（是一个8页的区间）
+    static const size_t SPANPAGES = 8; // 每次从页缓存获取的span大小，小需求固定拿 8 页，大需求按实际需要的页数拿
 
     CentralCache::CentralCache() {
         // 所有大小的内存块对应的空闲链表，初始为空 memory_order_relaxed - 不保证顺序，仅保证原子性
@@ -27,7 +27,78 @@ namespace MemoryPool {
         void* res = nullptr;
         try {
             res = centralFreeList_[index].load(std::memory_order_relaxed);
+
+            // 中心缓存为空
+            if (! res) {
+                // 向页缓存申请
+                size_t size = (index + 1) * ALIGNMENT;
+                res = fetchFromPageCache(size);
+
+                // 页缓存也为空
+                if (! res) {
+                    locks_[index].clear(std::memory_order_release);
+                    return nullptr;
+                }
+                // 将获取到的内存块分为小块（从页缓存申请来的是页区间）
+                char* start = static_cast<char*>(res);
+
+                // 实际分配到的页数 - 小于8页的需求分8页，大于8页的需求按实际去分
+                size_t numPages = (size <= SPANPAGES * PageCache::PAGE_SIZE) ?
+                                  SPANPAGES :
+                                  (size + PageCache::PAGE_SIZE - 1) / PageCache::PAGE_SIZE;
+                // 计算块数
+                size_t blockNum = (numPages * PageCache::PAGE_SIZE) / size;
+
+                if (blockNum > 1) {
+                    // 两个块才能构建空闲链表
+                    for (auto i = 1; i < blockNum; ++i) {
+                        void* cur = start + (i - 1) * size;
+                        void* next = start + i * size;
+                        *reinterpret_cast<void**>(cur) = next;
+                    }
+                    *reinterpret_cast<void**>(start + (blockNum - 1) * size) = nullptr;
+
+                    // 保存res的下一个节点
+                    void* next = *reinterpret_cast<void**>(res);
+                    *reinterpret_cast<void**>(res) = nullptr;
+
+                    // 更新中心缓存
+                    centralFreeList_[index].store(next,std::memory_order_release);
+
+                    // 无锁方式记录span信息，为归还内存到页缓存考虑
+                    size_t trackerIndex = spanCount_++;
+                    if (trackerIndex < spanTracks_.size()) { // 若追踪对象已满（最高1024个），则直接不记录 - 静默丢弃
+                        spanTracks_[trackerIndex].spanAddr.store(start,std::memory_order_release);
+                        spanTracks_[trackerIndex].numPages.store(numPages,std::memory_order_release);
+                        spanTracks_[trackerIndex].freeCount.store(blockNum,std::memory_order_release);
+                        spanTracks_[trackerIndex].blockCount.store(blockNum - 1,std::memory_order_release); // 第一个块res已被分配
+                    }
+                }
+             }
+
+            // 从中心缓存成功拿到所需大小的内存块
+            else {
+                void* next = *reinterpret_cast<void**>(res);
+                *reinterpret_cast<void**>(res) = nullptr;
+                centralFreeList_[index].store(next,std::memory_order_release);
+
+                // 更新span空闲计数
+                SpanTracker* tracker = getSpanTracker(res);
+                if (tracker) {
+                    // 减少一个空闲块
+                    tracker->freeCount.fetch_sub(1,std::memory_order_release);
+                }
+            }
         }
+        catch (...) {
+            locks_[index].clear(std::memory_order_release);
+            throw ;
+        }
+
+        //释放锁
+        locks_[index].clear(std::memory_order_release);
+
+        return res;
     }
 
     void CentralCache::returnRange(void *start, size_t size, size_t index) {
